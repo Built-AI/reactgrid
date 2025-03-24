c0b8a4ef3eec3d5e1e1fd6659f3f1bd8e5e8834c
Sodiq <sodiqridwan100@gmail.com>
2025-03-24 12:46:15 +0000

    chore: fix broken clipboard copy

--- src/lib/Functions/handleCopy.ts
+++ src/lib/Functions/handleCopy.ts
@@ -15,24 +15,26 @@ export function handleCopy(event: ClipboardEvent, state: State, removeValues = f
 }
 
 export function copyDataCommands(event: ClipboardEvent, state: State, div: HTMLDivElement): void {
-    const supportNavigatorClipboard = !!navigator?.clipboard?.write;
+  // how document.execCommand is deprecated, we need try use Clipboard API if is available
+  // https://developer.mozilla.org/en-US/docs/Web/API/Clipboard_API
+  const supportNavigatorClipboard = !!navigator?.clipboard?.write;
 
-    if (isBrowserSafari()) {
-        event.clipboardData.setData('text/html', div.innerHTML);
-    } else if (supportNavigatorClipboard) {
-        const clipboardItemData = {
-          "text/html": div.innerHtml,
-        }; 
-        const clipboardItem = new ClipboardItem(clipboardItemData);
-        await navigator.clipboard.write([clipboardItem]);
-    } else {
-        document.body.appendChild(div);
-        div.focus();
-        document.execCommand('selectAll', false);
-        document.execCommand('copy');
-        document.body.removeChild(div);
-    }
+  if (isBrowserSafari()) {
+    event.clipboardData.setData("text/html", div.innerHTML);
+  } else if (supportNavigatorClipboard) {
+    const clipboardItemData = {
+      "text/html": div.innerHTML,
+    };
+    const clipboardItem = new ClipboardItem(clipboardItemData);
+    navigator.clipboard.write([clipboardItem]).then(() => ({}));
+  } else {
+    document.body.appendChild(div);
+    div.focus();
+    document.execCommand("selectAll", false);
+    document.execCommand("copy");
+    document.body.removeChild(div);
+  }
 
-    state.hiddenFocusElement?.focus({ preventScroll: true });
-    event.preventDefault();
+  state.hiddenFocusElement?.focus({ preventScroll: true });
+  event.preventDefault();
 }
